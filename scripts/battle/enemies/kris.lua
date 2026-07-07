2979eee22110ec5ec146710e3790b7eadc15e9ff
Bli_Aik <haikun2333@126.com>
2026-07-07 19:06:41 +0800

    feat: 第七回合的开头矩形

--- scripts/battle/enemies/kris.lua
+++ scripts/battle/enemies/kris.lua
@@ -31,7 +31,7 @@ local RECHARGE_AVOID_WAVES = {
     [6] = true,
     [11] = true,
 }
-local FORCED_TURN = 10
+local FORCED_TURN = 7
 
 local function makeWaveList(first, last)
     local waves = {}
--- scripts/battle/waves/kris_phase1_07.lua
+++ scripts/battle/waves/kris_phase1_07.lua
@@ -1,11 +1,36 @@
 local KrisPhase1_07, super = Class(Wave)
 
+local RECT_DURATION = 20 / 60
+local RECT_WIDTH = 24
+local RECT_HEIGHT = SCREEN_HEIGHT * 1.4
+local RECT_ROTATION = math.rad(5)
+
 function KrisPhase1_07:init()
     super.init(self)
-    self.time = 5
+    self.time = 10
+    self.red_rect = nil
 end
 
 function KrisPhase1_07:onStart()
+    local rect = Rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RECT_WIDTH, RECT_HEIGHT)
+    rect:setOrigin(0.5, 0.5)
+    rect.color = { 1, 0, 0 }
+    rect.alpha = 1
+
+    self.red_rect = rect
+    self:addChild(rect)
+
+    self.timer:tween(RECT_DURATION, rect, {
+        rotation = RECT_ROTATION,
+        alpha = 0,
+    }, "linear", function()
+        if rect.parent then
+            rect:remove()
+        end
+        if self.red_rect == rect then
+            self.red_rect = nil
+        end
+    end)
 end
 
 function KrisPhase1_07:update()
