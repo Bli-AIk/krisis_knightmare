2c47816e7894a8b3a6304c5e239465c8274672f0
Bli_Aik <haikun2333@126.com>
2026-07-20 23:03:41 +0800

    refactor: 调整第九回合弹幕间隔

--- scripts/battle/waves/kris_phase1_09.lua
+++ scripts/battle/waves/kris_phase1_09.lua
@@ -2,11 +2,11 @@ local KrisPhase1_09, super = Class("kris_phase1_02")
 
 function KrisPhase1_09:init()
     super.init(self)
-    self.time = self.time - 3
+    self.time = self.time - 1.5
 end
 
 function KrisPhase1_09:getSlashInterval()
-    return 25 / 60
+    return 35 / 60
 end
 
 function KrisPhase1_09:getKrisSlashAnimationFrameDelay()
